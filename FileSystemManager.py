5631609a92f071666de2180b328517b3152e215f
Sagar Chadha <sagarchadha@gmail.com>
2021-10-04 12:05:40 -0400

    feat: implement file system manager

---
+++ FileSystemManager.py
@@ -0,0 +1,46 @@
+from math import ceil
+
+class FileSystemManager:
+  capacity = 0
+  blockSize = 1024
+  freeBlocks = []
+  usedBlocks = {}
+
+  def __init__(self, capacity: int=10):
+    super().__init__()
+    self.capacity = capacity
+    self.freeBlocks = [i for i in range(self.capacity)]
+
+  def calculateNumBlocks(self, fileSize: int) -> int:
+    numBlocks = ceil(fileSize/self.blockSize)
+
+    if numBlocks > len(self.freeBlocks):
+      raise ValueError(f'Not enough space for file with size of {fileSize} bytes')
+    
+    return numBlocks
+
+  def save(self, fileId: str, fileSize: str) -> list:
+    if fileId in self.usedBlocks:
+      raise ValueError('Error: File already exists')
+
+    numBlocks = self.calculateNumBlocks(int(fileSize))
+
+    newBlocks = []
+    for _ in range(numBlocks):
+      newBlocks.append(self.freeBlocks.pop())
+    self.usedBlocks[fileId] = newBlocks
+    return newBlocks
+
+  def read(self, fileId: str) -> list:
+    if fileId not in self.usedBlocks:
+      raise ValueError('Error: File does not exist')
+
+    return self.usedBlocks[fileId]
+
+  def delete(self, fileId: str) -> None:
+    if fileId not in self.usedBlocks:
+      raise ValueError('Error: File does not exist')
+    
+    deletedBlocks = self.usedBlocks[fileId]
+    self.freeBlocks = self.freeBlocks + deletedBlocks
+    del self.usedBlocks[fileId]
